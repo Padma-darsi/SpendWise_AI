fbcba780e220c32d34c54949b945907782894c11
Padma Darsi <padmadarsi230@gmail.com>
2026-01-26 19:31:11 +0530

    edited frontend files

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "spend-wise-ai-psi.vercel.app/api"
+  baseURL: "https://spend-wise-ai-psi.vercel.app/api"
 });
 
 axiosInstance.interceptors.request.use((config) => {
